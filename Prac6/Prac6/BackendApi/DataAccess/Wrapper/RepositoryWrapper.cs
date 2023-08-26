234885f15f42cc6bf0eb4d1140ba15a06c95a128
LizaBereza <125023360+LizaBereza@users.noreply.github.com>
2023-08-26 19:37:16 +0300

    Project Tour

---
+++ Prac6/Prac6/BackendApi/DataAccess/Wrapper/RepositoryWrapper.cs
@@ -0,0 +1,37 @@
+﻿using Domain1.Interfaces;
+using DataAccess.Models;
+using DataAccess.Repositories;
+
+
+namespace DataAccess.Wrapper
+{
+    public class RepositoryWrapper : IRepositoryWrapper
+    {
+        private ShopsContext _repoContext;
+
+        private IUserRepository _user;
+        public IUserRepository User
+        {
+            get
+            {
+                if (_user == null)
+                {
+                    _user = new UserRepository(_repoContext);
+                }
+                return _user;
+            }
+
+        }
+
+
+        public RepositoryWrapper(ShopsContext repositoryContext)
+        {
+            _repoContext = repositoryContext;
+        }
+
+        public async Task Save()
+        {
+           await _repoContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ Prac6/Prac6/BackendApi/Domain1/Interfaces/IRepositoryWrapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain1.Interfaces;
+using Domain1.Models;
+
+namespace Domain1.Wrapper
+{
+    public interface IRepositoryWrapper
+    {
+        IUserRepository User { get; }
+        Task Save();
+    }
+}
